6c1ae402cedf891191fb6260392abbe1c2c5df1f
Gabriel28072004 <127897278+Gabriel28072004@users.noreply.github.com>
2023-10-14 11:17:07 -0300

    Add files via upload
    Slide 04, Questão 1

---
+++ aprovado_reprovado.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+
+
+
+
+int main(){
+
+ int N, i, Q;
+
+/* Slide 04, Questão 01
+
+ Letra a
+
+ printf("Digite sua nota: ");
+  scanf("%d", &N);
+
+ if (N < 0 || N > 100){
+    printf("Nota invalida.");
+ }
+
+ else if (N >= 60){
+    printf("APROVADO");
+ }
+ else{
+   printf("REPROVADO");
+ }
+
+*/
+
+/*
+Letra B
+
+do {
+
+ printf("Digite uma nota: ");
+  scanf("%d", &N);
+
+
+
+ if (N >= 60){
+    printf("APROVADO\n");
+
+ }
+ else{
+    printf("REPROVADO\n");
+ }
+
+
+}while (N > 0 && N < 100);
+*/
+
+/*
+
+Letra C
+
+printf("Deseja informar as notas de quantos alunos? ");
+ scanf("%d", &Q);
+
+for (i = 0; i < Q; i++){
+
+    printf("Digite sua nota: ");
+     scanf("%d", &N);
+
+ if (N < 0 || N > 100){
+    printf("Nota invalida.");
+ }
+
+ else if (N >= 60){
+    printf("APROVADO\n");
+ }
+ else{
+   printf("REPROVADO\n");
+ }
+
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
